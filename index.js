6fc81ff915e6c144bd248446e30bcebce908b430
unknown <wachirawit_fact@hotmail.com>
2019-03-10 22:22:14 +0700

    edit code

--- index.js
+++ index.js
@@ -7,7 +7,17 @@ const config = require('./config.json');
 // create LINE SDK client
 const client = new line.Client(config);
 const app = express();
-var redis_client = require('redis').createClient(process.env.REDIS_URL);
+//var redis_client = require('redis').createClient(process.env.REDIS_URL);
+var redis = require('redis');
+var redis_Client = redis.createClient({host : 'localhost', port : 6379});
+
+redisClient.on('ready',function() {
+ console.log("Redis is ready");
+});
+
+redisClient.on('error',function() {
+ console.log("Error in Redis");
+});
 
 // webhook callback
 app.post('/webhook', line.middleware(config), (req, res) => {
