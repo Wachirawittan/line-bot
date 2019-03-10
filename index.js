9222036730c7a23b4d34abda9d315234929a92a0
unknown <wachirawit_fact@hotmail.com>
2019-03-10 20:13:07 +0700

    edit code

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+'use strict';
+
 const line = require('@line/bot-sdk');
 const express = require('express');
 const config = require('./config.json');
+
 // create LINE SDK client
 const client = new line.Client(config);
-var redis = require("redis"),
-    redis_client = redis.createClient(process.env.REDIS_URL);
+
 const app = express();
+
 // webhook callback
 app.post('/webhook', line.middleware(config), (req, res) => {
   // req.body.events should be an array of events
@@ -28,6 +31,7 @@ app.post('/webhook', line.middleware(config), (req, res) => {
       res.status(500).end();
     });
 });
+
 // simple reply function
 const replyText = (token, texts) => {
   texts = Array.isArray(texts) ? texts : [texts];
@@ -36,6 +40,7 @@ const replyText = (token, texts) => {
     texts.map((text) => ({ type: 'text', text }))
   );
 };
+
 // callback function to handle a single event
 function handleEvent(event) {
   switch (event.type) {
@@ -57,46 +62,57 @@ function handleEvent(event) {
         default:
           throw new Error(`Unknown message: ${JSON.stringify(message)}`);
       }
+
     case 'follow':
       return replyText(event.replyToken, 'Got followed event');
+
     case 'unfollow':
       return console.log(`Unfollowed this bot: ${JSON.stringify(event)}`);
+
     case 'join':
       return replyText(event.replyToken, `Joined ${event.source.type}`);
+
     case 'leave':
       return console.log(`Left: ${JSON.stringify(event)}`);
+
     case 'postback':
       let data = event.postback.data;
       return replyText(event.replyToken, `Got postback: ${data}`);
+
     case 'beacon':
       const dm = `${Buffer.from(event.beacon.dm || '', 'hex').toString('utf8')}`;
       return replyText(event.replyToken, `${event.beacon.type} beacon hwid : ${event.beacon.hwid} with device message = ${dm}`);
+
     default:
       throw new Error(`Unknown event: ${JSON.stringify(event)}`);
   }
 }
+
 function handleText(message, replyToken) {
   return replyText(replyToken, message.text);
 }
+
 function handleImage(message, replyToken) {
   return replyText(replyToken, 'Got Image');
 }
+
 function handleVideo(message, replyToken) {
   return replyText(replyToken, 'Got Video');
 }
+
 function handleAudio(message, replyToken) {
   return replyText(replyToken, 'Got Audio');
 }
+
 function handleLocation(message, replyToken) {
   return replyText(replyToken, 'Got Location');
 }
+
 function handleSticker(message, replyToken) {
   return replyText(replyToken, 'Got Sticker');
 }
-const port = process.env.PORT;
 
-
-
+const port = config.port;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
 });
