63bc5f7b1b9e27958d4a62943d8a844dd4978e1c
WACHIRAWIT TANASINSAWAT <wachirawit.tanasinsawat@ibm.com>
2019-03-13 18:54:31 +0700

    rollback

---
+++ index_test.js
@@ -0,0 +1,111 @@
+'use strict';
+
+const line = require('@line/bot-sdk');
+const express = require('express');
+const config = require('./config.json');
+
+// create LINE SDK client
+const client = new line.Client(config);
+const app = express();
+
+var rtg   = require("url").parse(process.env.REDIS_URL);
+var redis = require("redis").createClient(rtg.port, rtg.hostname);
+redis.auth(rtg.auth.split(":")[1]);
+console.log(process.env.REDIS_URL);
+redis.on('connect', function() {
+    console.log('Redis client connected');
+});
+
+// webhook callback
+app.post('/webhook', line.middleware(config), (req, res) => {
+  // req.body.events should be an array of events
+  if (!Array.isArray(req.body.events)) {
+    return res.status(500).end();
+  }
+  // handle events separately
+  Promise.all(req.body.events.map(event => {
+    console.log('event', event);
+    // check verify webhook event
+    if (event.replyToken === '00000000000000000000000000000000' ||
+      event.replyToken === 'ffffffffffffffffffffffffffffffff') {
+      return;
+    }
+    return handleEvent(event);
+  }))
+    .then(() => res.end())
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+// simple reply function
+const replyText = (token, texts) => {
+  texts = Array.isArray(texts) ? texts : [texts];
+  return client.replyMessage(
+    token,
+    texts.map((text) => ({ type: 'text', text }))
+  );
+};
+
+// callback function to handle a single event
+function handleEvent(event) {
+  switch (event.type) {
+    case 'message':
+      const message = event.message;
+      switch (message.type) {
+        case 'text':
+          return handleText(message, event.replyToken);
+        default:
+          throw new Error(`Unknown message: ${JSON.stringify(message)}`);
+      }
+    default:
+      throw new Error(`Unknown event: ${JSON.stringify(event)}`);
+  }
+}
+
+
+function getresult(input_Message){
+  redis.get(input_Message, function (error, result) {
+    console.log('GET result inredis ->' + result);
+  });
+  return result;
+}
+
+function handleText(message, replyToken) {
+  var esultTextreply;
+  getresult(message.text).then((result)=>{
+    console.log('GET result outredis->' + result);
+    if(result!=null&&result!=''){
+      resultTextreply = "จำนวนแคลลอรี่ของ "+message.text+" เท่ากับ "+resultText+" แคลลอรี่";
+    }else{
+      resultTextreply = message.text;
+    }
+  });
+  return replyText(replyToken, resultTextreply);
+}
+
+function handleImage(message, replyToken) {
+  return replyText(replyToken, 'Got Image');
+}
+
+function handleVideo(message, replyToken) {
+  return replyText(replyToken, 'Got Video');
+}
+
+function handleAudio(message, replyToken) {
+  return replyText(replyToken, 'Got Audio');
+}
+
+function handleLocation(message, replyToken) {
+  return replyText(replyToken, 'Got Location');
+}
+
+function handleSticker(message, replyToken) {
+  return replyText(replyToken, 'Got Sticker');
+}
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
